0e155d7eceee7bec93002310c819908185d353e1
pabesco <36999515+pabesco@users.noreply.github.com>
2022-09-11 23:05:59 +0530

    Update ImageColorInvert.py
    Converted the code to a function which can be used to invert input image file colors.

--- ImageColorInvert.py
+++ ImageColorInvert.py
@@ -1,9 +1,12 @@
 from PIL import Image, ImageChops
 import cv2
-img = cv2.imread("NewMaze2.jpg")
-print(img.shape)
-img = Image.open('NewMaze2.jpg')
-inv_img = ImageChops.invert(img)
-#inv_image = 255 - img
-inv_img.show()
-inv_img.save("NewMaze2BW.jpg")
+
+def ImageColorInvert(file):
+    img = cv2.imread(file)
+    print(img.shape)
+    img = Image.open(file)
+    inv_img = ImageChops.invert(img)
+    #inv_image = 255 - img
+    inv_img.show()
+    inv_img.save(f"{file}_Invert.jpg")
+    return f"{file}_Invert.jpg"
